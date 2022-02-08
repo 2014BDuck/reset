82e02a48d803352fda3904a0b9729e15cd5cfa38
Jiekun <jiekun.dev@gmail.com>
2022-02-08 08:53:37 +0800

    added search a 2d matrix series

---
+++ 240-search-a-2d-matrix-ii.go
@@ -0,0 +1,44 @@
+// Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+// Integers in each row are sorted in ascending from left to right.
+// Integers in each column are sorted in ascending from top to bottom.
+
+// Example 1:
+
+// Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+// Output: true
+// Example 2:
+
+// Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+// Output: false
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= n, m <= 300
+// -109 <= matrix[i][j] <= 109
+// All the integers in each row are sorted in ascending order.
+// All the integers in each column are sorted in ascending order.
+// -109 <= target <= 109
+
+func searchMatrix(matrix [][]int, target int) bool {
+	n := len(matrix)
+	if n < 1 {
+		return false
+	}
+	m := len(matrix[0])
+
+	i, j := n-1, 0
+
+	for i >= 0 && j < m {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] > target {
+			i--
+		} else {
+			j++
+		}
+	}
+	return false
+}
