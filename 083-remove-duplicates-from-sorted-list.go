1914b8040b9778059b663faa353a91384309085d
Jiekun <jiekun.dev@gmail.com>
2022-05-23 08:24:26 +0800

    added 083 remove duplicates from sorted list

---
+++ 083-remove-duplicates-from-sorted-list.go
@@ -0,0 +1,40 @@
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+// Example 1:
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+// Example 2:
+
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 300].
+// -100 <= Node.val <= 100
+// The list is guaranteed to be sorted in ascending order.
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+    last := 101
+    node := head
+    dummy := &ListNode{}
+    prev := dummy
+    for node != nil {
+        if node.Val != last {
+            last = node.Val
+            prev.Next = node
+            prev = node
+        }
+        node = node.Next
+    }
+    prev.Next = nil
+    return head
+}
